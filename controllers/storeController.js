58414d5bb9a974973915bc888a09a0364f46b56e
MT <mtrojas@protonmail.com>
2020-08-29 17:15:04 -0400

    chore(cleanup): delete test middleware.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,11 +1,3 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = 'Maite';
-  if(req.name === 'Maite') {
-    throw Error('I don\'t like that name');
-  }
-  next();
-};
-
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
