6b963ba6645c4ec61529f2b38c53d7e57b4f40c2
MT <mtrojas@protonmail.com>
2020-10-09 20:10:58 -0300

    fix(point): set the location data to be a point before updating the store.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -35,6 +35,8 @@ exports.editStore = async (req, res) => {
 }
 
 exports.updateStore = async (req, res) => {
+  // Set the location data to be a point
+  req.body.location.type = 'Point';
   // 1. Find and update the store
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true, // return the new store instead of the old one
